60929f525ba7f51f6c5b150151bfa7e31961df10
Gazerbeam <37074836+Gazerbeam7@users.noreply.github.com>
2022-08-07 23:20:09 +0200

    initial commit

---
+++ views/home_view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php include_once 'views/includes/head.php' ?>
+    <title><?= ucfirst($page) ?> - Exercice</title>
+</head>
+
+<body>
+    <?php include_once 'views/includes/header.php' ?>
+
+    <h1 class="homeH1">ICI C'est l'accueil</h1>
+    <img src="assets/images/img.jpg" alt="img test" class="imgTest1">
+
+    <?php include_once 'views/includes/footer.php' ?>
+    <script src="assets/js/app.js"></script>
+</body>
+
+</html>
---
+++ views/includes/header.php
@@ -0,0 +1,7 @@
+<header>
+    <ul>
+        <li><a href="index.php?page=home">home</a></li>
+        <li><a href="index.php?page=alpha">ALPHA</a></li>
+
+    </ul>
+</header>
