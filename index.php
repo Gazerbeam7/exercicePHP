7bac68b35884178339fc9dd8c44ff11fec493370
Rinzler <theocharron@protonmail.com>
2022-08-08 22:47:07 +0200

    Ajout DB CONFIG

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
+include_once '_config/config.php';
+include_once '_config/db.php';
+
+
 // définition de la page courante
-if (isset($_GET['page']) AND !empty($_GET['page'])) {
+if (isset($_GET['page']) and !empty($_GET['page'])) {
     $page = trim(strtolower($_GET['page']));
 } else {
     $page = 'home';
@@ -10,12 +14,12 @@ if (isset($_GET['page']) AND !empty($_GET['page'])) {
 //array contenant toutes les pages
 $allPages = scandir('controllers/');
 
-if (in_array($page.'_controller.php', $allPages)) {
+if (in_array($page . '_controller.php', $allPages)) {
 
     //inclusion de la page
-    include_once 'models/'.$page.'_model.php';
-    include_once 'controllers/'.$page.'_controller.php';
-    include_once 'views/'.$page.'_view.php';
+    include_once 'models/' . $page . '_model.php';
+    include_once 'controllers/' . $page . '_controller.php';
+    include_once 'views/' . $page . '_view.php';
 } else {
     echo 'Erreur 404';
 }
--- views/includes/head.php
+++ views/includes/head.php
@@ -1,11 +1,20 @@
-<meta charset="utf-8">
-<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="Exercice">
-<meta name="Gazerbeam" content="Exercice">
+<!-- Meta Tags -->
+<meta charset="UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=0, maximum-scale=1, initial-scale=1.0, maximum-scale=1">
+<meta name="author" content="<?= WEBSITE_AUTHOR ?>">
+<meta name="description" content="<?= WEBSITE_DESCRIPTION ?>" />
+<meta name=”keywords” content="<?= WEBSITE_KEYWORDS ?>" />
+<meta name="Reply-to" content="<?= WEBSITE_AUTHOR_MAIL ?>">
+<meta name="Copyright" content="<?= WEBSITE_AUTHOR ?>">
+<meta name="Language" content="<?= WEBSITE_LANGUAGE ?>">
 
-<!--<link rel="icon" href="/favicon.ico">-->
-<!--<link rel="icon" href="/favicon.svg" type="image/svg+xml">-->
-<!--<link rel="apple-touch-icon" href="/apple-touch-icon.png">-->
+<!-- Open Graph tags -->
+<meta property="og:type" content="website" />
+<meta property="og:url" content="<?= WEBSITE_FACEBOOK_URL ?>" />
+<meta property="og:title" content="<?= WEBSITE_FACEBOOK_NAME ?>" />
+<meta property="og:description" content="<?= WEBSITE_FACEBOOK_DESCRIPTION ?>" />
+<meta property="og:image" content="<?= WEBSITE_FACEBOOK_IMAGE ?>" />
 
-<link rel="stylesheet" href="assets/css/style.css">
+<!-- CSS Styles -->
+<link rel="stylesheet" href="<?= PATH ?>assets/css/style.css" />
